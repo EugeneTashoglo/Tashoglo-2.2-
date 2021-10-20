f01b37b19342a334acc0fad447699846a091ead3
user5 <user5@09.ru>
2021-10-20 13:28:58 +0200

    Добавьте файлы проекта.

---
+++ Tashoglo 2.2 Средний/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Tashoglo_2._2_Средний
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите a:");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите b:");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите c:");
+            double c = double.Parse(Console.ReadLine());
+            if (a == b || b == c || a == c) Console.WriteLine("Имеется");
+            else Console.WriteLine("Не имеется");
+        }
+    }
+}
